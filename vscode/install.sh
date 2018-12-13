0a02dd8c6780503223bc64a928e7b95bae61742c
Thomas Nairn <thomasnairn@Thomass-MacBook-Pro.local>
2018-12-13 01:43:59 +0000

    Uncommented vscode install

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,8 +1,6 @@
-# cat "$(dirname -- "$0")/extensions" | while read -r line ; do
-#     echo "Installing $line"
-#     code --install-extension $line --force
-# done
-
-echo "Linking vscode"
+cat "$(dirname -- "$0")/extensions" | while read -r line ; do
+    echo "Installing $line"
+    code --install-extension $line --force
+done
 
 $ZSH/bin/link-file  "$HOME/.vscode.conf" "$HOME/Library/Application Support/Code/User"
