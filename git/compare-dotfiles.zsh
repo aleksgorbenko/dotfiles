84169e56878f833e98399887aa71d9bff663acaf
Thomas Nairn <thomas@thenairn.com>
2018-12-13 11:12:45 +0000

    Routine changes

--- git/compare-dotfiles.zsh
+++ git/compare-dotfiles.zsh
@@ -14,7 +14,7 @@ warn () {
   printf "\r  [\033[00;33mWARN\033[0m] $1\n"
 }
 
-if [ -n "$(git --git-dir=$ZSH/.git status --porcelain)" ]; then
+if [ -n "$(git --git-dir=$ZSH/.git --work-tree=$ZSH status --porcelain)" ]; then
 warn "There are unstaged changes to your dotfiles!"
 warn ">> cdf"
 fi
