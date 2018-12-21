32fb77bce561683ea5507ffcddc61d1d6de133fc
Thomas Nairn <thomas@thenairn.com>
2018-12-21 00:14:41 +0000

    log

--- vscode/backup.zsh
+++ vscode/backup.zsh
@@ -8,12 +8,21 @@ EXT_TO_REMOVE_COUNT=$(echo $EXT_TO_REMOVE  | wc -l)
 EXT_TO_INSTALL=$(comm -13 <(echo $INSTALLED) <(echo $GIT_EXTS))
 EXT_TO_INSTALL_COUNT=$(echo $EXT_TO_INSTALL | wc -l)
 
+syncVSCode() {
+    $ZSH/bin/log_user "Installing $EXT_TO_INSTALL_COUNT extensions"
+    echo $EXT_TO_INSTALL | while read -r line ; do
+        echo "Installing $line"
+        code --install-extension $line --force
+    done
+    $ZSH/bin/log_user "Uninstalled $EXT_TO_REMOVE_COUNT extensions"
+}
+
 if ((EXT_TO_INSTALL_COUNT > 0 && EXT_TO_REMOVE_COUNT > 0)) {
     $ZSH/bin/log_user "VSCode out of sync, sync now?"
     read -n 1 response < /dev/tty
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
     then
-        
+        syncVSCode
     fi
 }
 
