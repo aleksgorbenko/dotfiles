aa6de2d55d48bf68b0d0d30f8d0eb68481563f0a
Thomas Nairn <thomas@thenairn.com>
2018-12-21 00:02:32 +0000

    Added extension comparison to VSCode

--- vscode/backup.zsh
+++ vscode/backup.zsh
@@ -1,11 +1,21 @@
 SOURCE_LOC=$_
 EXT_FILE="$(dirname -- "$SOURCE_LOC")/extensions"
+GIT_EXTS="$(sort $ZSH/vscode/extensions)"
+INSTALLED="$(sort <(code --list-extensions))"
+
+EXT_TO_REMOVE=$(comm -23 <(echo $INSTALLED) <(echo $GIT_EXTS))
+EXT_TO_INSTALL=$(comm -13 <(echo $INSTALLED) <(echo $GIT_EXTS))
+
 
 backupCodeExt() {
-    code --list-extensions > $EXT_FILE
+    $INSTALLED > $EXT_FILE
     async_stop_worker vscodebkp
 }
 
+compareCode() {
+    grep -Fxv $INSTALLED `sort $ZSH/vscode/extensions`
+}
+
 async_init
-async_start_worker vscodebkp -n
-async_job vscodebkp backupCodeExt
+# async_start_worker vscodebkp -n
+# async_job vscodebkp backupCodeExt
