155cbdb23b1056abe2acd9b337961a86251e1c44
Thomas Nairn <thomas@thenairn.com>
2019-01-03 17:18:39 +0000

    Routine changes

--- vscode/pre-commit.zsh
+++ vscode/pre-commit.zsh
@@ -1,18 +1,15 @@
 backupCodeExt() {
-    local EXT_FILE="$(dirname -- "$SOURCE_LOC")/extensions"
-    local INSTALLED="$(sort <(code --list-extensions))"
-    $INSTALLED > $EXT_FILE
-}
-
-$ZSH/bin/log_user "Backup VSCode extensions?"
-read -n 1 response < /dev/tty
-
-if [ -z "$PS1" ]; then
-    $ZSH/bin/log_warn "Skipping VSCode syncing as not interactive."
-    else
+    $ZSH/bin/log_user "Backup VSCode extensions?"
     read -n 1 response < /dev/tty
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
     then
-        backupCodeExt
+        local EXT_FILE="$(dirname -- "$SOURCE_LOC")/extensions"
+        local INSTALLED="$(sort <(code --list-extensions))"
+        $INSTALLED > $EXT_FILE
     fi
-fi
+}
+
+case "$-" in
+*i*)  backupCodeExt ;;
+*)	$ZSH/bin/log_warn "Skipping VSCode syncing as not interactive." ;;
+esac
