4c019396f7ac4cd6a5d4e9d9760eeeef54a1b60b
aleksgbk <aleksedgorbenko@gmail.com>
2019-06-19 21:12:10 +0100

    added a few more .... aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -13,6 +13,8 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+alias .......="cd ../../../../../.."
 alias ~="cd ~" # `cd` is probably faster to type though
 alias -- -="cd -"
 
