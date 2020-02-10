f185b674264b9d8a4afaa1c22efc61e75548278d
aleks_gorbenko <aleks.gbko@gmail.com>
2020-02-10 13:58:40 +0000

    centralised path config

--- system/path.zsh
+++
@@ -1,2 +0,0 @@
-# zsh, brew and other binaries
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,1 +1,11 @@
+# zsh, brew and other binaries
+export PATH="$ZSH/bin:$PATH:/usr/local/bin"
 export PATH="/usr/local/opt/gettext/bin:$PATH"
+export GO111MODULE=on
+export GOPATH=$HOME/go
+export PATH="$GOPATH/bin:$PATH"
+
+if (( $+commands[yarn] ))
+then
+  export PATH="$PATH:`yarn global bin`"
+fi
