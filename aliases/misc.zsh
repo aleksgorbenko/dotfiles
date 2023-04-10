d04073bd982447897514b1771995ced7976cbad4
aleks_gorbenko <aleks.gbko@gmail.com>
2023-04-10 13:42:12 +0100

    chore: Add functions to sync dropbox and icloud obsidian book notes

--- aliases/misc.zsh
+++ aliases/misc.zsh
@@ -7,3 +7,9 @@ _mkc() {
   #compdef mkc
   _files -W "$1" -/
 }
+
+## mosk
+
+alias mosk-books1="cd /Users/gbko/Dropbox/mosk/ && git pull origin master"
+alias mosk-books2="git add . && git commit -m \"chore: Add book notes\" && git push origin master"
+alias mosk-books3="cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/mosk/ && git pull origin master"
