198878b07aa36d715153193852ae63bdf8921d20
gbko <aleks.gbko@gmail.com>
2025-07-04 22:46:37 +0800

    chore: Update 04 Jul 2025

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -3,7 +3,6 @@ source "${ZSH_ALIASES}/git.zsh"
 source "${ZSH_ALIASES}/docker.zsh"
 source "${ZSH_ALIASES}/k8s.zsh"
 source "${ZSH_ALIASES}/terraform.zsh"
-source "${ZSH_ALIASES}/misc.zsh"
 
 # Keep your localised aliases in the ~/.aliases.
 # These are relevant for the local machine only (work or neighbours laptop)
