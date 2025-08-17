6f98d9b2564044e62c0b0f05eed1dcbd6de0216f
gbko <aleks.gbko@gmail.com>
2025-08-17 14:50:04 +0800

    chore: Add brew and mas aliases

--- aliases/system.zsh
+++ aliases/system.zsh
@@ -44,15 +44,34 @@ alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ipinfo="curl ipinfo.io | jq"
 
+# BREW
+
 alias b="brew"
+alias bls="brew list"
+alias blsc="brew list --cask"
+alias blsl="brew list --formulae"
+alias bcl="brew cleanup"
 alias bb="brew bundle --file $HOME/.dotfiles/Brewfile"
+
 alias bi="brew install"
 alias bic="brew install --cask"
+
+alias bup="brew update" # update brew itself
 alias bu="brew upgrade"
 alias buc="brew upgrade --cask"
+
 alias brmc="brew uninstall --cask"
 alias brm="brew uninstall"
-alias bup="brew update"
+
+# MAS (Mac App Store)
+
+alias m="mas"
+alias mi="mas install"
+alias mo="mas open" # open Mac App Store app page
+alias ms="mas search"
+alias mu="mas upgrade"
+alias mls="mas list" # list installed apps
+alias mrm="mas remove"
 
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/me/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
